b38a74d2ed96653297bce19472c121f486f18333
Tojokely <rtojosoa15@alustudent.com>
2016-07-14 08:43:17 +0000

    Final

--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 var express = require('express');
+var moment = require('moment');
+var path = require('path');
+
 var app = express();
 var port = 8080;
 
-app.get('/', function(req,res){
-    res.send('Hello World');
+app.get('/:timeStamp', function(req,res){
+    var myDate;
+    if (/^\d{8,}$/.test(req.params.timeStamp)) {
+        myDate = moment(req.params.timeStamp, "X");
+    }
+    else {
+        myDate = moment(req.params.timeStamp, "MMMM D, YYYY");
+    }
+    
+     if(myDate.isValid()) {
+    res.json({
+      unix: myDate.format("X"),
+      natural: myDate.format("MMMM D, YYYY")
+    });
+  } else {
+    res.json({
+      unix: null,
+      natural: null
+    });
+  }
+});
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(port, function(){
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 8080!');
 });
